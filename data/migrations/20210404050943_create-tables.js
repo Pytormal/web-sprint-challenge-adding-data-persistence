db6ec954d28f2cb97d86f4a4440845eeee23ed32
AlexAndrew Mallory <pytormal@live.com>
2021-04-04 00:13:28 -0700

    added database tables

--- data/migrations/20210404050943_create-tables.js
+++ data/migrations/20210404050943_create-tables.js
@@ -9,21 +9,31 @@ exports.up = function (knex) {
     .createTable("resources", (tbl) => {
       tbl.increments("resource_id");
       tbl.string("resource_name").notNullable().unique();
-      tbl.text("project_description", 1000).notNullable();
+      tbl.text("resource_description", 1000).notNullable();
     })
     .createTable("tasks", (tbl) => {
       tbl.increments("task_id");
-      tbl.text("project_description", 1000).notNullable();
+      tbl.text("task_description", 1000).notNullable();
       tbl.text("task_notes", 1000).notNullable();
       tbl.boolean("task_completed").notNullable();
-      tbl.bigint("project_id").unsigned().references("project.id");
+      tbl
+        .bigint("project_id")
+        .unsigned()
+        .references("project_id")
+        .inTable("projects");
     })
     .createTable("project_resources", (tbl) => {
       tbl.increments();
-      tbl.bigint("project_name").unsigned().references("project.name");
-      tbl.bigint("resource_id").unsigned().references("resource.id");
-      //   tbl.date('date')
-      //   tbl.primary(["project_name", "resource_id", 'date']);
+      tbl
+        .bigint("project_name")
+        .unsigned()
+        .references("project_name")
+        .inTable("projects");
+      tbl
+        .bigint("resource_id")
+        .unsigned()
+        .references("resource_id")
+        .inTable("resources");
     });
 };
 
