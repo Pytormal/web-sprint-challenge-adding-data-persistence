1ab2671cc4a5f34b9d4606ebe89617a34b145288
AlexAndrew Mallory <pytormal@live.com>
2021-04-03 23:33:59 -0700

    added database tables

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require("express");
+const helmet = require('helmet');
+
+
+const server = express();
+// const pRouter = require("./project/router.js");
+// const rRouter = require("./resource/router.js");
+// const tRouter = require("./task/router.js");
+
+server.use(helmet());
+server.use(express.json());
+// server.use("/api/project", pRouter);
+// server.use("/api/resource", rRouter);
+// server.use("/api/task", tRouter);
+
+server.get("/", (req, res) => {
+  res.json({ hello: "Sprint Project 4.2" });
+});
+
+module.exports = server;
---
+++ data/migrations/20210404050943_create-tables.js
@@ -0,0 +1,36 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name", 128).notNullable().unique();
+      tbl.text("project_description", 1000).notNullable();
+      tbl.boolean("project_completed").notNullable();
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name").notNullable().unique();
+      tbl.text("project_description", 1000).notNullable();
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.text("project_description", 1000).notNullable();
+      tbl.text("task_notes", 1000).notNullable();
+      tbl.boolean("task_completed").notNullable();
+      tbl.bigint("project_id").unsigned().references("project.id");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments();
+      tbl.bigint("project_name").unsigned().references("project.name");
+      tbl.bigint("resource_id").unsigned().references("resource.id");
+      //   tbl.date('date')
+      //   tbl.primary(["project_name", "resource_id", 'date']);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
