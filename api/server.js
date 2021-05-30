86d09b444603278807663430251c7ea934d345bd
AlexAndrew Mallory <pytormal@live.com>
2021-05-30 08:59:13 -0700

    started server and installed some dependencies

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ hello: "Sprint Project 4.2" });
+});
+
+module.exports = server;
